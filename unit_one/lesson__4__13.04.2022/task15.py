c32e4b8090b9b5e73c3d4923aa19318844c0b88d
sergey_kruchkov <seerozha777@gmail.com>
2022-04-15 19:36:43 +0300

    перезалив задачи 15

--- unit_one/lesson__4__13.04.2022/task15.py
+++ unit_one/lesson__4__13.04.2022/task15.py
@@ -1,5 +1,5 @@
 # Написать игру "Поле чудес"
-
+"""
 Отгадываемые слова и описание лежат в разных  массивах по одинаковому индексу.
 words = ["оператор", "конструкция", "объект"]
 desc_  = [ "Это слово обозначает наименьшую автономную часть языка программирования", "..", ".." ]
@@ -24,3 +24,39 @@ O  ▒  ▒  ▒  ▒  ▒  O  ▒
 Нет такой буквы.
 У вас осталось 9 попыток !
 Введите букву:
+"""
+
+import random
+
+print(f"***** В ЭФИРЕ КАПИТАЛ-ШОУ 'ПОЛЕ ЧУДЕС' *****")
+
+words = ["пенал", "ручка", "линейка"]
+discrip = ["Есть у школьника", "Подружка карандаша", "Прибор измерения"]
+
+while True:
+    index = random.randint(0, len(words)-1)
+    guessed_word = words[index]
+    hashed_guessed_word = ["#"] * len(guessed_word)
+    attempts = 10
+    print(f"Внимание вопрос.\n{discrip[index]}. Что это? Вы можете ошибиться {attempts} раз(раза)\n{hashed_guessed_word}")
+
+    while attempts > 0:
+        proposed_letter = input("Введите букву: ")
+        if proposed_letter in guessed_word:
+            hashed_guessed_word[guessed_word.index(proposed_letter)] = proposed_letter
+            print(hashed_guessed_word)
+            if "#" not in hashed_guessed_word:
+                print(f"Поздравляем! Вы отгадали слово '{guessed_word}'!")
+                break
+        elif proposed_letter not in guessed_word and attempts == 1:
+            print(f"К сожалению, вы не угадали слово '{guessed_word}'")
+            break
+        elif proposed_letter not in guessed_word:
+            print(f"Нет такой буквы. Количество оставшихся попыток: {attempts - 1}")
+            attempts -= 1
+
+    answer = input("\nХотите продолжить? Введите 'y' английское, если да; или любой иной символ - если нет: ")
+    if answer == "y":
+        continue
+    elif answer != "y":
+        break
